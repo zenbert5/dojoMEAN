d0479184e994eddc5843def921f68b023331c424
Shawn Chen <shawnchen@nexus7-Mac.local>
2018-10-01 09:38:44 -0700

    update mean

---
+++ MEAN/promiseExample1.js
@@ -0,0 +1,37 @@
+var isMomHappy = true;
+
+// Promise
+var willIGetNewPhone = new Promise(
+    function (resolve, reject) {
+        if (isMomHappy) {
+            var phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone); // fulfilled
+        } else {
+            var reason = new Error('mom is not happy');
+            reject(reason); // reject
+        }
+
+    }
+);
+
+// call our promise
+var askMom = function () {
+    willIGetNewPhone
+        .then(function (fulfilled) {
+            // yay, you got a new phone
+            console.log(fulfilled);
+        // output: { brand: 'Samsung', color: 'black' }
+        })
+        .catch(function (error) {
+            // oops, mom don't buy it
+            console.log(error.message);
+        // output: 'mom is not happy'
+        });
+};
+
+askMom();
+
+
