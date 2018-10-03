143348b3d4d6cabae842f9402f22cac25d35a2d3
Shawn Chen <shawnchen@nexus7-Mac.local>
2018-10-03 08:17:52 -0700

    efscats

--- express/carsNCats/server.js
+++ express/carsNCats/server.js
@@ -6,8 +6,8 @@
  * 
  *  using embedded js - ejs for rendering routes
  *  available routes are /cars, cats, and /form
+ *  default to index.html
  */
-
 
 var express = require("express");
 console.log("Let's find out what express is", express);
@@ -22,6 +22,10 @@ app.set('view engine', 'ejs');
 app.use('/static', express.static(__dirname + '/static'));
 
 console.log("Let's find out what app is", app);
+
+app.use(function(req, res){
+    res.sendfile('./views/index.html');
+});
 // use app's get method and pass it the base route '/' and a callback
 /* app.get('/', function(request, response) {
     // just for fun, take a look at the request and response objects
---
+++ express/ejsCats/server.js
@@ -0,0 +1,74 @@
+/*
+ *  MEAN - express ejs cats
+ *  Oct 2, 2018
+ *  shawn chen
+ *  codingDojo
+ * 
+ */
+
+var express = require("express");
+console.log("Let's find out what express is", express);
+// invoke express and store the result in the variable app
+var app = express();
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+// this is the line that tells our server to use the "/static" folder for static contentcopy
+app.use('/static', express.static(__dirname + '/static'));
+
+console.log("Let's find out what app is", app);
+
+/* app.use(function(req, res){
+    res.redirect('/cats');
+}); */
+
+app.get('/cats', function (request, response){
+    var showCats = [
+        { tag: 'cat1', cat: '/static/images/cat1.jpeg', url: '/cats/1' },
+        { tag: 'cat2', cat: '/static/images/cat2.jpeg', url: '/cats/2' },
+        { tag: 'cat3', cat: '/static/images/cat3.jpg', url: '/cats/3' },
+    ];
+    response.render('index', {cats: showCats });
+})
+
+app.get('/cats/1', function (request, response){
+    var cat = {
+        name: 'Curly',
+        food: 'Fish', 
+        age: 7,
+        spots: ['In front of the fireplace', 'Catnap Sack'],
+        url: '/static/images/cat1.jpeg'
+    };
+    response.render('cat', {cat: cat});
+})
+
+app.get('/cats/2', function (request, response){
+    var cat = {
+        name: 'Maddy',
+        food: 'Sushi', 
+        age: 3,
+        spots: ['Swimming Pool Deck', 'Roof'],
+        url: '/static/images/cat2.jpeg'
+    };
+    response.render('cat', {cat: cat});
+})
+
+app.get('/cats/3', function (request, response){
+    var cat = {
+        name: 'Crazy',
+        food: 'Junk Food', 
+        age: 1,
+        spots: ['Under the bed', 'Joe\'s hair', 'Top of can'],
+        url: '/static/images/cat3.jpg'
+    };
+    response.render('cat', {cat: cat});
+})
+
+
+// tell the express app to listen on port 8000, always put this at the end of your server.js file
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
+
