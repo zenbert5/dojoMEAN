4ffb8973ea6d6ed090f83de88d48c0627b90b3d0
Shawn Chen <shawnchen@nexus7-Mac.local>
2018-10-18 08:43:45 -0700

    tweaked some code

--- angular/teamManager/team/src/app/app.component.ts
+++ angular/teamManager/team/src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from './http.service';
+import { Component } from '@angular/core';
+//import { HttpService } from './http.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'Team Manager';
 
-    constructor(private _httpService: HttpService) { }
-    ngOnInit() {
-    }
+    //constructor(private _httpService: HttpService) { }
 }
